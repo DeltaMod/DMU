fixed adjust tick to correctly evaluate ticks after scaling axis

DMU/plot_utils.py
@@ -101,38 +101,54 @@ def get_combined_legend(FIG):
     return(flatten(leg_handles),flatten(leg_labels))
 
 # Automatically adjust tick locations to intervals that do not require two decimal places
-def adjust_ticks(ticks):
+def adjust_ticks(ticks,axrange=None,numticks=8):
     nums      = []
     ooms      = []
     
     #First, we find out the highest oom that we will base all our nums on
+    if axrange == None:
+        axrange = [float(ticks[-1]),float( ticks[0])]
+        
     for tick in ticks:
         if tick != 0:
             ooms.append(np.floor(np.log10(abs(tick))))
     oom = np.max(ooms)
     
-    nums = np.array(ticks)/10**oom
+    nums = np.array(ticks)/(10**oom)
     diffs = np.diff(nums)
     
-    def diffnum_converter(diff,nums):
-        proxval = [0.1,0.2,0.5,1.0,2.0]
-        diffnum = min(proxval, key=lambda x: abs(x - diff))
+    axrange = axrange/(10**oom)
+    diffnum = (axrange[1]-axrange[0])/numticks
+    
+    def diffnum_converter(diff,nums,numticks):
+        proxval = [0.01,0.02,0.05,0.1,0.2,0.5,1.0,2.0,5.0,10,20,50,100]
+        
+        diffnum = min(proxval, key=lambda x: abs(x - diff))  
         new_low =  nums[0] - (nums[0] % diffnum)
         new_high = (nums[-1] + diffnum) - (nums[-1] % diffnum)
         
-        if len(np.round(np.arange(new_low-diffnum,new_high+diffnum,diffnum),2)) >=12:
+        if len(np.arange(new_low,new_high,diffnum)) >=numticks+2:
             try:
                 diffnum = proxval[proxval.index(diffnum) + 1]
             except:
-                diffnum = diffnum*2
+                diffnum = 2*diffnum
+            
+            new_low =  nums[0] - (nums[0] % diffnum)
+            new_high = (nums[-1] + diffnum) - (nums[-1] % diffnum)
+            
+            if len(np.arange(new_low,new_high,diffnum)) >=8:
+                try:
+                    diffnum = proxval[proxval.index(diffnum) + 1]
+                except:
+                    diffnum = diffnum*2
                 
             new_low =  nums[0] - (nums[0] % diffnum)
             new_high = (nums[-1] + diffnum) - (nums[-1] % diffnum)
-        
+            
         new_nums = np.round(np.arange(new_low-5*diffnum,new_high+5*diffnum,diffnum),2)
         return(new_nums)
 
-    new_nums = diffnum_converter(diffs[0],nums)
+    new_nums = diffnum_converter(diffnum,nums,numticks)
     
     for num in new_nums:
         new_ticks = new_nums * np.power(10,oom)

DMU/utils.py
@@ -326,39 +326,12 @@ def bias_plotter(data,FIG,**kwargs):
                         for ax in FIG.ax:
                             bbox = FIG.ax[ax].get_position()
                             
-                            bbox.x0 = kw.bounds_padding[0]; bbox.x1 = kw.bounds_padding[1];
-                            bbox.y0 = kw.bounds_padding[2]; bbox.y1 = kw.bounds_padding[3];
+                            bbox.x0 = kw.bounds_padding[0]; bbox.x1 = kw.bounds_padding[1]; 
                             
                             FIG.ax[ax].set_position(bbox)
                             bboxes[ax] = bbox
 
-
-                        TICKS = []
-                        for key in FIG.ax.keys():
-                            TICKS.append(FIG.ax[key].get_yticks())
-
-                        align_axis_zeros([FIG.ax[0],FIG.ax[1],FIG.ax[2]])
-
-                        for i,oticks in enumerate(TICKS):
-                            current_ylim = FIG.ax[i].get_ylim()                         #Get current limits
-                            ticks = adjust_ticks(oticks)                                #adjust ticks based on original ticks
-                            FIG.ax[i].set_yticks(ticks)                                 #set new tick locations
-                            FIG.ax[i].set_yticklabels([f"{val:.1e}" for val in ticks])  #set new ticklabels
-                            #use the scalar formatter 
-                            yfmt = ScalarFormatterForceFormat()
-                            yfmt.set_powerlimits((-2,2))
-                            FIG.ax[i].yaxis.set_major_formatter(yfmt)
-                            FIG.ax[i].set_ylim(current_ylim)
-
-                        ticklabelwidth = dummy_text_params("−0.00",FIG,fontsize=plt.rcParams["ytick.labelsize"],usetex=plt.rcParams["text.usetex"])["width"] # Get the width of the bounding box in figure coordinates
-                        #We will get the width of a single "-" in figure coordinates too.
-                        minuslabelwidth = dummy_text_params("−",FIG,fontsize=plt.rcParams["ytick.labelsize"],usetex=plt.rcParams["text.usetex"])["width"] # Get the width of the bounding box in figure coordinates
                         
-                        FIG.ax[2].spines.right.set_position(("outward",FIG.fig.dpi*5*ticklabelwidth))
-                        
-                        
-
-
                         #We want to set axis limits so that Voltage = 90% of the ylim
                         def rpad(data,ratio):
                             drange = np.max(data)-np.min(data)
@@ -378,12 +351,31 @@ def bias_plotter(data,FIG,**kwargs):
 
                         FIG.ax[0].set_ylim(rpad(Det_I,0.8*RatioMod)[0])
                         FIG.ax[1].set_ylim(rpad(Em_I,0.85*RatioMod)[0])
-                        #%%    
+                        
                         align_axis_zeros([FIG.ax[0],FIG.ax[1],FIG.ax[2]])
                         
+                        TICKS = []
+                        for key in FIG.ax.keys():
+                            TICKS.append(FIG.ax[key].get_yticks())
+
+                        align_axis_zeros([FIG.ax[0],FIG.ax[1],FIG.ax[2]])
+
+                        for i,oticks in enumerate(TICKS):
+                            current_ylim = FIG.ax[i].get_ylim()                         #Get current limits
+                            ticks = adjust_ticks(oticks,current_ylim)                   #adjust ticks based on original ticks
+                            FIG.ax[i].set_yticks(ticks)                                 #set new tick locations
+                            FIG.ax[i].set_yticklabels([f"{val:.1e}" for val in ticks])  #set new ticklabels
+                            #use the scalar formatter 
+                            yfmt = ScalarFormatterForceFormat()
+                            yfmt.set_powerlimits((-2,2))
+                            FIG.ax[i].yaxis.set_major_formatter(yfmt)
+                            FIG.ax[i].set_ylim(current_ylim)
+
+                        ticklabelwidth = dummy_text_params("−0.00",FIG,fontsize=plt.rcParams["ytick.labelsize"],usetex=plt.rcParams["text.usetex"])["width"] # Get the width of the bounding box in figure coordinates
+                        #We will get the width of a single "-" in figure coordinates too.
+                        minuslabelwidth = dummy_text_params("−",FIG,fontsize=plt.rcParams["ytick.labelsize"],usetex=plt.rcParams["text.usetex"])["width"] # Get the width of the bounding box in figure coordinates
                         
-                        
-                        
+                        FIG.ax[2].spines.right.set_position(("outward",FIG.fig.dpi*5*ticklabelwidth))
                         
                         
                         #Setting Spine colours and tickparameters