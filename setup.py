c1953370456520af7399e722d0a0d9da4984473c
Atli Vidar Már Flodgren <33879770+DeltaMod@users.noreply.github.com>
2024-10-25 16:30:07 +0200

    updated version number for new patch

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ installREQ = [[item for item in installREQ[0] if item not in ["logging"]]]
 
 setup(
     name="DMU",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(),
     scripts=[TrgtScr],
 
