8970f16507bc79a33af6961cefde0a3670e87219
Atli Vidar Már Flodgren <33879770+DeltaMod@users.noreply.github.com>
2026-06-01 15:38:27 +0200

    Must write standardised kwarg/dict entry handoff
    This handoff should be able to:
    
    take xrange/xmin-xmax
    If users provide a dict, it should take the values of the dict only if they are different from a default dict (whose defafults should be the same as what is in the args)
    Unsure how best to do this
    
    Should I pre-prepare a dict entry for all types of monitors and structures, or should I provide a barebones one that contains shared properties, and then let the user extract them from the object once created and modify them after?
    Difficult choices.

--- DMU/lumutils/analysis/monitors.py
+++ DMU/lumutils/analysis/monitors.py
@@ -81,13 +81,12 @@ class AnalysisMixin(hlp.HelpersMixin):
 
         return self._register_monitor(adict, name, group)
             
-    def add_analysis(self, atype, name, group=None, xyz=(0,0,0), xrange=1, yrange=1, zrange=0,prop_dict={},bounds = None):
+    def add_analysis(self, atype, name, group=None, xyz=(0,0,0), xrange=1, yrange=1, zrange=0,prop_dict={}):
         """
         Add a 2D moviemonitor
         
         Two modes: span or range. You may provide either xrange=5 or xrange=[-2.5,2.5].
         Parameters:
-            sim       : Lumerical simulation object
             name      : Name of  monitor
             group     : format: "model::group1::group2"
             xyz       : centre coordinates (tuple) - if using min/max, this can be anything
@@ -108,4 +107,34 @@ class AnalysisMixin(hlp.HelpersMixin):
         self._set_monitor_bounds(adict, skip)
         self.select_and_set_props(None, prop_dict)
         return self._register_monitor(adict, name, group)
-            
+    
+    def add_FDTD(self,name="FDTD",xyz=(0,0,0),xrange=1e-6,yrange=1e-6,zrange=1e-6,prop_dict=None):
+        """
+        Add a FDTD simulation object
+        
+        Two modes: span or range. You may provide either xrange=5 or xrange=[-2.5,2.5].
+        Parameters:
+            name      : Name of  monitor
+            xyz       : centre coordinates (tuple) - if using min/max, this can be anything
+            xrange    : float or [min,max] for x
+            yrange    : float or [min,max] for y
+            zrange    : float or [min,max] for z
+            prop_dict : dict object, example dict:
+                prop_dict = {
+                    "x":0,"y":0,"z":0,
+                    "x span":1e-6,"y span":1e-6,"z span":1e-6,
+                    "x min":-0.5e-6,"x max":0.5e-6,
+                    "y min":-0.5e-6,"y max":0.5e-6,
+                    "z min":-0.5e-6,"z max":0.5e-6,
+                    "mesh type":"auto non-uniform", 
+                    "mesh accuracy":3, "dt stability factor": 0.5,
+                    "mesh refinement":"conformal variant 1",
+                    "min mesh step": 0.00025,
+                    "x min bc":"PML","x max bc":"PML",
+                    "y min bc":"PML","y max bc":"PML",
+                    "z min bc":"PML","z max bc":"PML",
+                    "auto shutoff min":1e-5, "auto shutoff max":10000
+                    }
+        """
+        xyz = tuple(xyz)
+        adict,monitor_type,skip = hlp.determine_2D_3D_spans_normals(xyz,xrange,yrange,zrange,allow3D=True)
